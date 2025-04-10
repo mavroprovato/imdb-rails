a24333c6bcff4067c18b9eb742b73af815eca9e7
Kostas Kokkoros <mavroprovato@gmail.com>
2025-04-11 01:29:40 +0300

    Refactor etl

--- app/services/etl.rb
+++ app/services/etl.rb
@@ -2,27 +2,14 @@
 
 class Etl
   def load
-    puts "Downloading person data"
-    response = Faraday.get("https://datasets.imdbws.com/name.basics.tsv.gz")
-    filename = "#{Rails.root}/tmp/name.basics.tsv.gz"
-    File.open(filename, mode: "wb") do |file|
-      file.write(response.body)
-    end
-    puts "Person data downloaded"
+    filename = extract
     puts "Loading person data"
     person_data = []
     File.open(filename, mode: "rb") do |file|
       Zlib::GzipReader.new(file).each_line.with_index do |line, index|
         next if index == 0
 
-        data = line.split("\t")
-        person_data << {
-          unique_id: data[0],
-          name: data[1],
-          birth_year: data[2] == '\N' ? nil : data[2].to_i,
-          death_year: data[3] == '\N' ? nil : data[3].to_i
-        }
-
+        person_data << transform_person_data(line)
         if index % 10_000 == 0
           Person.import person_data, validate: false
           person_data = []
@@ -31,5 +18,29 @@ class Etl
     end
     Person.import person_data
     puts "Person data loaded."
+  end
+
+  private
+
+  def transform_person_data(line)
+    data = line.split("\t")
+
+    {
+      unique_id: data[0],
+      name: data[1],
+      birth_year: data[2] == '\N' ? nil : data[2].to_i,
+      death_year: data[3] == '\N' ? nil : data[3].to_i
+    }
+  end
+
+  def extract
+    puts "Downloading person data"
+    response = Faraday.get("https://datasets.imdbws.com/name.basics.tsv.gz")
+    filename = "#{Rails.root}/tmp/name.basics.tsv.gz"
+    File.open(filename, mode: "wb") do |file|
+      file.write(response.body)
+    end
+    puts "Person data downloaded"
+    filename
   end
 end
