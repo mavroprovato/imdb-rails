ff91cd6203efbb9c49216f9b223aa3bfd3ab4dc7
Kostas Kokkoros <mavroprovato@gmail.com>
2025-04-24 22:21:25 +0300

    Cleanup models

--- app/models/genre_title.rb
+++
@@ -1,4 +0,0 @@
-class GenreTitle < ApplicationRecord
-  belongs_to :genre
-  belongs_to :title
-end
--- app/models/title_genre.rb
+++ app/models/title_genre.rb
@@ -1,2 +1,4 @@
 class TitleGenre < ApplicationRecord
+  belongs_to :genre
+  belongs_to :title
 end
