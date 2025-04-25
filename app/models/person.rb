3064cf513c0859a46b7fe6385d1e4e3d3d9a63de
Kostas Kokkoros <mavroprovato@gmail.com>
2025-04-25 15:27:46 +0300

    Add person model test

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+  validates :unique_id, presence: true, uniqueness: true
+  validates :name, presence: true
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:person) { create(:person) }
+
+  it { is_expected.to validate_presence_of(:unique_id) }
+  it { is_expected.to validate_uniqueness_of(:unique_id) }
+  it { is_expected.to validate_presence_of(:name) }
 end
