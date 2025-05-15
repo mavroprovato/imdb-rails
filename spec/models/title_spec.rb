a1276be85ddccc922450e8f5d63b4886878a600c
Kostas Kokkoros <mavroprovato@gmail.com>
2025-05-16 02:18:05 +0300

    Fix Layout/EmptyLineAfterGuardClause and Layout/FirstHashElementIndentation

--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.describe Title do
   subject(:title) { create(:title) }
 
+  let(:title_type_enum_values) do
+    {
+      movie: "movie", short: "short", tv_episode: "tvEpisode", tv_mini_series: "tvMiniSeries", tv_movie: "tvMovie",
+      tv_pilot: "tvPilot", tv_series: "tvSeries", tv_short: "tvShort", tv_special: "tvSpecial", video: "video",
+      video_game: "videoGame"
+    }
+  end
+
   it { is_expected.to validate_presence_of(:unique_id) }
   it { is_expected.to validate_uniqueness_of(:unique_id) }
   it { is_expected.to validate_presence_of(:title_type) }
 
   it {
-    expect(title).to define_enum_for(:title_type).with_values({
-      movie: "movie", short: "short", tv_episode: "tvEpisode", tv_mini_series: "tvMiniSeries", tv_movie: "tvMovie",
-      tv_pilot: "tvPilot", tv_series: "tvSeries", tv_short: "tvShort", tv_special: "tvSpecial", video: "video",
-      video_game: "videoGame"
-    }).backed_by_column_of_type(:enum)
+    expect(title).to define_enum_for(:title_type).with_values(title_type_enum_values).backed_by_column_of_type(:enum)
   }
 
   it { is_expected.to validate_presence_of(:title) }
