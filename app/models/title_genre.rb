261cb80f060a8da127467d9d47e0555c9f7e97ec
Kostas Kokkoros <mavroprovato@gmail.com>
2025-05-16 01:22:15 +0300

    Fix Rails/RedundantPresenceValidationOnBelongsTo

--- app/models/title_genre.rb
+++ app/models/title_genre.rb
@@ -6,7 +6,5 @@ class TitleGenre < ApplicationRecord
   belongs_to :title
   belongs_to :genre
 
-  validates :title_id, presence: true
-  validates :genre_id, presence: true
   validates :title_id, uniqueness: { scope: :genre_id }
 end
