7ce2dc5f1be0296c5e40ef0061a4e79e57c73eb5
Kostas Kokkoros <mavroprovato@gmail.com>
2025-05-20 23:15:19 +0300

    Remove fail test

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,5 +8,4 @@ RSpec.describe Person do
   it { is_expected.to validate_presence_of(:unique_id) }
   it { is_expected.to validate_uniqueness_of(:unique_id) }
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:name) }
 end
