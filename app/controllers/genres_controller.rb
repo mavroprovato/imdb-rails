5ec681fc8da8639bee462d4dae6ed39a0d5beb44
Kostas Kokkoros <mavroprovato@gmail.com>
2025-05-23 20:11:08 +0300

    Add pagination

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,6 +3,37 @@
 # The genre controller
 class GenresController < ApplicationController
   def index
-    render json: Genre.all
+    render json: paginated_results
+  end
+
+  protected
+
+  def model
+    Genre
+  end
+
+  private
+
+  def paginated_results
+    {
+      total:,
+      results:
+    }
+  end
+
+  def results
+    paginate_query order_query model
+  end
+
+  def order_query(query)
+    query.order(:id)
+  end
+
+  def paginate_query(query)
+    query.limit(10).offset(0)
+  end
+
+  def total
+    model.count
   end
 end
