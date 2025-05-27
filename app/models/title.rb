a9f0d2ee39f63c94a3b9f68970a175e2d2834710
Kostas Kokkoros <mavroprovato@gmail.com>
2025-05-27 20:43:26 +0300

    Add more tests for title model

--- app/models/title.rb
+++ app/models/title.rb
@@ -2,11 +2,13 @@
 
 # The title model
 class Title < ApplicationRecord
-  enum :title_type, {
+  # Possible values of the title type enum
+  TITLE_TYPE_VALUES = {
     movie: 'movie', short: 'short', tv_episode: 'tvEpisode', tv_mini_series: 'tvMiniSeries', tv_movie: 'tvMovie',
     tv_pilot: 'tvPilot', tv_series: 'tvSeries', tv_short: 'tvShort', tv_special: 'tvSpecial', video: 'video',
     video_game: 'videoGame'
-  }
+  }.freeze
+  enum :title_type, TITLE_TYPE_VALUES
 
   has_many :title_genres, dependent: :destroy
   has_many :genres, through: :title_genres
--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -5,25 +5,19 @@ require 'rails_helper'
 RSpec.describe Title do
   subject(:title) { create(:title) }
 
-  let(:title_type_enum_values) do
-    {
-      movie: 'movie', short: 'short', tv_episode: 'tvEpisode', tv_mini_series: 'tvMiniSeries', tv_movie: 'tvMovie',
-      tv_pilot: 'tvPilot', tv_series: 'tvSeries', tv_short: 'tvShort', tv_special: 'tvSpecial', video: 'video',
-      video_game: 'videoGame'
-    }
-  end
-
   it { is_expected.to validate_presence_of(:unique_id) }
   it { is_expected.to validate_uniqueness_of(:unique_id) }
   it { is_expected.to validate_presence_of(:title_type) }
 
   it {
-    expect(title).to define_enum_for(:title_type).with_values(title_type_enum_values).backed_by_column_of_type(:enum)
+    expect(title).to define_enum_for(:title_type).with_values(Title::TITLE_TYPE_VALUES).backed_by_column_of_type(:enum)
   }
 
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:original_title) }
+  it { is_expected.to allow_values(true, false).for(:adult) }
   it { is_expected.to validate_presence_of(:runtime) }
+
   it { is_expected.to have_many(:title_genres) }
   it { is_expected.to have_many(:genres).through(:title_genres) }
 end
