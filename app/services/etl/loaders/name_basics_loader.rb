c6e5299c08ced04d81a33b6f426f701201aa0758
Kostas Kokkoros <mavroprovato@gmail.com>
2025-06-22 02:58:37 +0300

    Refactor the name basics loader

--- app/services/etl/loaders/name_basics_loader.rb
+++ app/services/etl/loaders/name_basics_loader.rb
@@ -21,29 +21,40 @@ module Etl
       #
       # @param batch Array[Hash] The data to load.
       def process_data(batch)
-        process_professions(batch)
-        process_people(batch)
-        process_primary_professions(batch)
-        process_person_known_for_title(batch)
+        load_professions(batch)
+        load_people(batch)
+        load_primary_professions(batch)
+        load_person_known_for_title(batch)
       end
 
       private
 
       attr_reader :loaded_professions, :loaded_people, :loaded_titles
 
-      def profession_data(batch)
+      # Transform the profession data.
+      #
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The transformed profession data.
+      def transform_profession_data(batch)
         read_unique_values(batch, :primaryProfession, multivalued: true).each_with_object([]) do |name, array|
           array << { name: }
         end
       end
 
-      def process_professions(batch)
-        Profession.import profession_data(batch), validate: false, on_duplicate_key_ignore: true
+      # Load the +Profession+ data to the database.
+      #
+      # @param batch Array[Hash] The batch data.
+      def load_professions(batch)
+        Profession.import transform_profession_data(batch), validate: false, on_duplicate_key_ignore: true
         @loaded_professions = loaded_values(
           Profession, :name, read_unique_values(batch, :primaryProfession, multivalued: true)
         )
       end
 
+      # Transform each input row from the data file in order to be loaded into a +Person+ model.
+      #
+      # @param row Hash The data file input row.
+      # @return Hash The transformed data.
       def transform_person_row(row)
         {
           unique_id: row[:nconst],
@@ -53,50 +64,83 @@ module Etl
         }
       end
 
-      def person_data(batch)
+      # Transform the person data for each batch.
+      #
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The data to load.
+      def transform_person_data(batch)
         batch.map { |row| transform_person_row(row) }
       end
 
-      def process_people(batch)
-        Person.import person_data(batch), validate: false, on_duplicate_key_update: {
+      # Load the +Person+ data to the database.
+      #
+      # @param batch Array[Hash] The data to load.
+      def load_people(batch)
+        Person.import transform_person_data(batch), validate: false, on_duplicate_key_update: {
           conflict_target: [:unique_id], columns: %i[name birth_year death_year]
         }
         @loaded_people = loaded_values(Person, :unique_id, read_unique_values(batch, :nconst))
       end
 
-      def person_primary_profession_data(batch)
+      # Transform each input row in order to be loaded into a +PersonPrimaryProfession+ model.
+      #
+      # @param row Hash The data to load.
+      # @return Array[Hash] The transformed data.
+      def transform_person_primary_profession_row(row)
+        row[:primaryProfession].split(',').each_with_object([]) do |profession, array|
+          array << { person_id: loaded_people[row[:nconst]], profession_id: loaded_professions[profession] }
+        end
+      end
+
+      # Transform the person primary profession data for each batch.
+      #
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The data to load.
+      def transform_person_primary_professions(batch)
         batch.reject { |row| row[:primaryProfession] == NULL_VALUE }.each_with_object([]) do |row, array|
-          row[:primaryProfession].split(',').map do |profession|
-            array << { person_id: loaded_people[row[:nconst]], profession_id: loaded_professions[profession] }
-          end
+          array.concat transform_person_primary_profession_row(row)
         end
       end
 
-      def process_primary_professions(batch)
-        PersonPrimaryProfession.import person_primary_profession_data(batch), validate: false,
-                                                                              on_duplicate_key_ignore: true
+      # Load the +PersonPrimaryProfession+ data to the database.
+      #
+      # @param batch Array[Hash] The batch data.
+      def load_primary_professions(batch)
+        PersonPrimaryProfession.import transform_person_primary_professions(batch), validate: false,
+                                                                                    on_duplicate_key_ignore: true
       end
 
-      def process_person_known_for_title(batch)
-        @loaded_titles = loaded_values(Title, :unique_id, read_unique_values(batch, :knownForTitles, multivalued: true))
-        PersonKnownForTitle.import person_known_for_title_data(batch), validate: false,
-                                                                       on_duplicate_key_ignore: true
+      # Transform each input row in order to be loaded into a +PersonKnownForTitle+ model.
+      #
+      # @param row Hash The data file input row.
+      # @return Array[Hash] The transformed data.
+      def transform_person_known_for_title_row(row)
+        row[:knownForTitles].split(',').each_with_object([]) do |title, array|
+          if loaded_titles[title].nil?
+            Rails.logger.warn "Title #{title} not loaded"
+            next
+          end
+          array << { person_id: loaded_people[row[:nconst]], title_id: loaded_titles[title] }
+        end
       end
 
-      def transform_person_known_for_title_row(row, title_id)
-        { person_id: loaded_people[row[:nconst]], title_id: }
-      end
-
-      def person_known_for_title_data(batch)
+      # Transform the person known for title data for each batch.
+      #
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The data to load.
+      def transform_person_known_for_title(batch)
         batch.reject { |row| row[:knownForTitles] == NULL_VALUE }.each_with_object([]) do |row, array|
-          row[:knownForTitles].split(',').each do |title|
-            if loaded_titles[title].nil?
-              Rails.logger.warn "Title #{title} not loaded"
-              next
-            end
-            array << transform_person_known_for_title_row(row, loaded_titles[title])
-          end
+          array.concat transform_person_known_for_title_row(row)
         end
+      end
+
+      # Load the +PersonKnownForTitle+ data to the database.
+      #
+      # @param batch Array[Hash] The batch data.
+      def load_person_known_for_title(batch)
+        @loaded_titles = loaded_values(Title, :unique_id, read_unique_values(batch, :knownForTitles, multivalued: true))
+        PersonKnownForTitle.import transform_person_known_for_title(batch), validate: false,
+                                                                            on_duplicate_key_ignore: true
       end
     end
   end
--- app/services/etl/loaders/title_basics_loader.rb
+++ app/services/etl/loaders/title_basics_loader.rb
@@ -19,7 +19,7 @@ module Etl
       # Process the data loaded from the title.basics.tsv.gz file, and loads them to the database. This class loads
       # values for the {#Genre}, {#Title} and {#TitleGenre} models.
       #
-      # @param batch Array[Hash] The data to load.
+      # @param batch Array[Hash] The batch data.
       def process_data(batch)
         load_genres(batch)
         load_titles(batch)
@@ -30,24 +30,26 @@ module Etl
 
       attr_reader :loaded_genres, :loaded_titles
 
-      # Transform the genre data.
+      # Transform the genre data for each batch.
       #
-      # @param batch Array[Hash] The data to load.
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The transformed genre data.
       def transform_genre_data(batch)
         read_unique_values(batch, :genres, multivalued: true).each_with_object([]) { |name, array| array << { name: } }
       end
 
       # Load the +Genre+ data to the database.
       #
-      # @param batch Array[Hash] The data to load.
+      # @param batch Array[Hash] The batch data.
       def load_genres(batch)
         Genre.import transform_genre_data(batch), validate: false, on_duplicate_key_ignore: true
         @loaded_genres = loaded_values(Genre, :name, read_unique_values(batch, :genres, multivalued: true))
       end
 
-      # Transform each input row in order to be load a +Title+ model.
+      # Transform each input row from the data file in order to be loaded into a +Title+ model.
       #
-      # @param row [Hash] The data to load.
+      # @param row Hash The data file input row.
+      # @return Hash The transformed data.
       def transform_title_row(row)
         {
           unique_id: row[:tconst],
@@ -61,38 +63,48 @@ module Etl
         }
       end
 
-      # Transform the title data.
+      # Transform the title data for each batch.
       #
-      # @param batch Array[Hash] The data to load.
-      def transform_title_data(batch)
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The data to load.
+      def transform_titles(batch)
         batch.map { |row| transform_title_row(row) }
       end
 
       # Load the +Title+ data to the database.
       #
-      # @param batch Array[Hash] The data to load.
+      # @param batch Array[Hash] The batch data.
       def load_titles(batch)
-        Title.import transform_title_data(batch), validate: false, on_duplicate_key_update: {
+        Title.import transform_titles(batch), validate: false, on_duplicate_key_update: {
           conflict_target: [:unique_id],
           columns: %i[title_type title original_title adult start_year end_year runtime]
         }
         @loaded_titles = loaded_values(Title, :unique_id, read_unique_values(batch, :tconst))
       end
 
-      # Transform the title genre data.
+      # Transform each input row from the data file in order to be loaded into a +TitleGenre+ model.
       #
-      # @param batch Array[Hash] The data to load.
+      # @param row Hash The data file input row.
+      # @return Array[Hash] The transformed data.
+      def transform_title_genre_row(row)
+        row[:genres].split(',').each_with_object([]) do |genre, array|
+          array << { title_id: loaded_titles[row[:tconst]], genre_id: loaded_genres[genre] }
+        end
+      end
+
+      # Transform the title genre data for each batch.
+      #
+      # @param batch Array[Hash] The batch data.
+      # @return Array[Hash] The data to load.
       def transform_title_genres(batch)
         batch.reject { |row| row[:genres] == NULL_VALUE }.each_with_object([]) do |row, array|
-          row[:genres].split(',').each do |genre|
-            array << { title_id: loaded_titles[row[:tconst]], genre_id: loaded_genres[genre] }
-          end
+          array.concat transform_title_genre_row(row)
         end
       end
 
       # Load the +TitleGenre+ data to the database.
       #
-      # @param batch Array[Hash] The data to load.
+      # @param batch Array[Hash] The batch data.
       def load_title_genres(batch)
         TitleGenre.import transform_title_genres(batch), validate: false, on_duplicate_key_ignore: true
       end
