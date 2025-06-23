052ba47435379bb4ddcf1ac2f3341c1a22566244
Kostas Kokkoros <mavroprovato@gmail.com>
2025-06-23 21:22:00 +0300

    Add test for TitlePrincipal category enum

--- spec/models/title_principal_spec.rb
+++ spec/models/title_principal_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe TitlePrincipal do
   it { is_expected.to belong_to(:title) }
   it { is_expected.to belong_to(:person) }
   it { is_expected.to validate_uniqueness_of(:person_id).scoped_to(:title_id, :ordering) }
+
+  it {
+    expect(title_principal).to define_enum_for(:principal_category).with_values(TitlePrincipal::CATEGORY_VALUES)
+                                                                   .backed_by_column_of_type(:enum)
+  }
 end
